b6ac407ac2a4e8f313b06dcbbd653244ac6d935d
zhang ze <david@www.david.com>
2016-06-14 16:57:25 +0800

    fix seed

--- db/seeds/len_f.rb
+++ db/seeds/len_f.rb
@@ -1,10 +1,10 @@
 
 max = Len::FLOAT_MAX
 #紧凑型C口镜头
-len_params=  [
-  { name: 'Emerald F-Mount 2.8/28-S', code: '1071609', weight: 517,
-    guang_quan_fan_wei_min: 2.8,  guang_quan_fan_wei_max: 22,
-    gong_zuo_ju_li_min: 233, gong_zuo_ju_li_max: 833,
-    gong_zuo_ju_li5_min: 0,  gong_zuo_ju_li5_max: 0,  # 0-0 N/A
-    gong_zuo_ju_li8_min: 0,  gong_zuo_ju_li8_max: 0
-  },
+#len_params=  [
+#  { name: 'Emerald F-Mount 2.8/28-S', code: '1071609', weight: 517,
+#    guang_quan_fan_wei_min: 2.8,  guang_quan_fan_wei_max: 22,
+#    gong_zuo_ju_li_min: 233, gong_zuo_ju_li_max: 833,
+#    gong_zuo_ju_li5_min: 0,  gong_zuo_ju_li5_max: 0,  # 0-0 N/A
+#    gong_zuo_ju_li8_min: 0,  gong_zuo_ju_li8_max: 0
+#  },
